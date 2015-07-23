5853777f99a4b0c2d7b3c5d26e73490cfa2bb228
Zlatimir Mihaylov <zlatimirmihailov@gmail.com>
2015-07-23 18:50:37 +0300

    added logic for the squares

--- ConnectTheDots/scripts/dots.js
+++ ConnectTheDots/scripts/dots.js
@@ -1,22 +1,31 @@
 var paper = Raphael('svg-container', 600, 600);
-
+var squares = [];
+var pathsIDs = 0;
 var rows = 10;
 var cols = 10;
 
 for (var i = 1; i <= rows; i++) {
     for (var j = 1; j <= cols; j++) {
-        var x = i * 40;
-        var y = j * 40;
+        var y = i * 40;
+        var x = j * 40;
 
-        // without last line ( they get out of the grid)
-        if (j !== rows) {
-            createLine(x, y, 40, 'v');
-        }
-        if (i !== cols) {
+        if (j !== cols) {
             createLine(x, y, 40, 'h');
         }
+    }
+}
 
-        createDot(x, y, 2);
+for (var i = 1; i <= rows; i++) {
+    for (var j = 1; j <= cols; j++) {
+        var y = i * 40;
+        var x = j * 40;
+
+        // without last line ( they get out of the grid)
+        if (i !== rows) {
+            createLine(x, y, 40, 'v');
+        }
+		
+		createDot(x, y, 2);
     }
 }
 
@@ -25,6 +34,7 @@ function createLine(x, y, len, type) {
     var line = paper.path(path)
     line.attr({stroke: 'lightgray'});
     line.attr({'stroke-width': '4'});
+	line.node.id = ++pathsIDs;
 }
 
 function createDot(x, y, r) {
@@ -34,16 +44,85 @@ function createDot(x, y, r) {
         cy: y,
         r: r
     });
+    //console.log(dot);
 }
 
-$('div svg path').mouseover(
-    function() {
-        $(this).attr({stroke: 'green'});
-        $(this).attr({'stroke-width': '5'});
+function mouseOver() {
+    $(this).attr({ stroke: 'green' });
+    $(this).attr({ 'stroke-width': '5' });
+    //console.log($(this).attr('id'));
+}
+
+function mouseOut() {
+    $(this).attr({ stroke: 'lightgray' });
+    $(this).attr({ 'stroke-width': '4' });
+}
+
+function clicked() {
+    $(this).attr({ stroke: 'red' });
+    $(this).attr({ 'stroke-width': '5' });
+    $(this).unbind("mouseover", mouseOver);
+    $(this).unbind("mouseout", mouseOut);
+
+    var currentLineID = +($(this).attr('id'));
+    //console.log(currentLineID);
+    addLineToSquare(currentLineID);
+    //console.log(squares[0]);
+}
+
+function addLineToSquare(id) {
+    squares.forEach(function (currentSquare) {
+		
+		if (!currentSquare.lines.length) {
+            return;
+        }
+	
+        var indexOfId = currentSquare.lines.indexOf(id);
+		
+        if (indexOfId >= 0) {
+			//console.log(currentSquare);
+            currentSquare.lines.splice(indexOfId, 1);
+            //console.log(currentSquare);
+        }
+		
+        if (!currentSquare.lines.length) {
+            // THE SQUARE HAS ALL SIDES FILLED, NEED TO CONTINUE THE LOGIC
+            console.log('IT WORKS');
+            //console.log(currentSquare);
+			//console.log(id);
+        }
+    });
+}
+
+$('div svg path')
+    .mouseover(mouseOver)
+    .mouseout(mouseOut)
+    .click(clicked);
+
+//console.log(paths);
+var lineOneStart = 1;
+var lineTwoStart = 10;
+var lineThreeStart = 91;
+var lineFourStart = 92;
+
+for (var i = 1; i <= 81; i++) {
+
+    var squaresLines = [lineOneStart, lineTwoStart, lineThreeStart, lineFourStart];
+
+    var square = {
+        lines: squaresLines
+    };
+
+    lineOneStart++;
+    lineTwoStart++;
+    lineThreeStart++;
+    lineFourStart++;
+
+    if (i % 9 == 0) {
+        lineThreeStart++;
+        lineFourStart++;
     }
-).mouseleave(
-    function() {
-        $(this).attr({stroke: 'lightgray'});
-        $(this).attr({'stroke-width': '5'});
-    }
-);
+    squares.push(square);
+}
+
+//console.log(squares[15]);
