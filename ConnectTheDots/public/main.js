8c1759f18500e5eaab36735dff0db3be8b9953d8
Elena Gancheva <eagan4eva@gmail.com>
2015-07-26 13:59:24 +0300

    project skeleton changed, public folder added. Sockets are working!

---
+++ ConnectTheDots/public/main.js
@@ -0,0 +1,148 @@
+var paper = Raphael('svg-container', 600, 600);
+var squares = [];
+var pathsIDs = 0;
+var rows = 10;
+var cols = 10;
+var socket = io();
+var currentLineID = -1;
+
+for (var i = 1; i <= rows; i++) {
+    for (var j = 1; j <= cols; j++) {
+        var y = i * 40;
+        var x = j * 40;
+
+        if (j !== cols) {
+            createLine(x, y, 40, 'h');
+        }
+    }
+}
+
+for (var i = 1; i <= rows; i++) {
+    for (var j = 1; j <= cols; j++) {
+        var y = i * 40;
+        var x = j * 40;
+
+        // without last line ( they get out of the grid)
+        if (i !== rows) {
+            createLine(x, y, 40, 'v');
+        }
+		
+		createDot(x, y, 2);
+    }
+}
+
+function createLine(x, y, len, type) {
+    var path = 'M' + x + ' ' + y + ' ' + type + len;
+    var line = paper.path(path)
+    line.attr({stroke: 'lightgray'});
+    line.attr({'stroke-width': '4'});
+	line.node.id = ++pathsIDs;
+}
+
+function createDot(x, y, r) {
+    var dot = paper.circle();
+    dot.attr({
+        cx: x,
+        cy: y,
+        r: r
+    });
+    //console.log(dot);
+}
+
+function mouseOver() {
+    $(this).attr({ stroke: 'green' });
+    $(this).attr({ 'stroke-width': '5' });
+    //console.log($(this).attr('id'));
+}
+
+function mouseOut() {
+    $(this).attr({ stroke: 'lightgray' });
+    $(this).attr({ 'stroke-width': '4' });
+}
+
+function clicked() {
+    $(this).attr({ stroke: 'red' });
+    $(this).attr({ 'stroke-width': '5' });
+    $(this).unbind("mouseover", mouseOver);
+    $(this).unbind("mouseout", mouseOut);
+
+    currentLineID = +($(this).attr('id'));
+    //console.log(currentLineID);
+    
+    socket.emit('id',currentLineID);
+    
+    socket.on('id',function (id) {
+        console.log(id);
+        addLineToSquare(id);
+      });
+    //console.log(squares[0]);
+}
+
+socket.emit('id',currentLineID);
+    
+socket.on('id',function (id) {
+   console.log(id);
+   addLineToSquare(id);
+});
+
+function addLineToSquare(id) {
+    squares.forEach(function (currentSquare) {
+		
+		if (!currentSquare.lines.length) {
+            return;
+        }
+        
+        var indexOfId = currentSquare.lines.indexOf(id);
+		
+        if (indexOfId >= 0) {
+			//console.log(currentSquare);
+            currentSquare.lines.splice(indexOfId, 1);
+            console.log(squares[0]);
+            //console.log(currentSquare);
+            
+            $('#' + id).unbind("mouseover", mouseOver);
+            $('#' + id).unbind("mouseout", mouseOut);
+            $('#' + id).attr({ stroke: 'red' });
+            $('#' + id).attr({ 'stroke-width': '5' });
+            
+        }
+		
+        if (!currentSquare.lines.length) {
+            // THE SQUARE HAS ALL SIDES FILLED, NEED TO CONTINUE THE LOGIC
+            console.log('IT WORKS');
+            //console.log(currentSquare);
+			//console.log(id);
+        }
+    });
+}
+
+$('div svg path')
+    .mouseover(mouseOver)
+    .mouseout(mouseOut)
+    .click(clicked);
+
+//console.log(paths);
+var lineOneStart = 1;
+var lineTwoStart = 10;
+var lineThreeStart = 91;
+var lineFourStart = 92;
+
+for (var i = 1; i <= 81; i++) {
+
+    var squaresLines = [lineOneStart, lineTwoStart, lineThreeStart, lineFourStart];
+
+    var square = {
+        lines: squaresLines
+    };
+
+    lineOneStart++;
+    lineTwoStart++;
+    lineThreeStart++;
+    lineFourStart++;
+
+    if (i % 9 == 0) {
+        lineThreeStart++;
+        lineFourStart++;
+    }
+    squares.push(square);
+}
---
+++ ConnectTheDots/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  socket.on('id',function(id) {
+    console.log('id: ' + id);
+    io.emit('id',id);
+  });
+});
+
