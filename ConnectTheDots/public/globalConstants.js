38ee7e886103aee6090f739e8cd6b19e5c1532ce
hpslavov <slavov.hristo@gmail.com>
2015-07-29 18:31:39 +0300

    property values of lightning.js exported to globalConstants.js as constants

--- ConnectTheDots/public/globalConstants.js
+++ ConnectTheDots/public/globalConstants.js
@@ -29,11 +29,44 @@ var constants = (function() {
         height: 40
     };
 
+    var lightningSize = {
+        width: 128,
+        height: 118
+    };
+
+    var lightningCoordinates = {
+        x: 0,
+        y:0
+    };
+
+    var lightningFrameOptions = {
+        frames: 30,
+        renderSpeed: 18,
+        frameCounter: 0,
+        tickCounter: 0,
+        renderEndPoint:0
+    };
+
+    var lightningUpdates = {
+        lightningDelta: Math.random(),
+        lightningUpdate:900
+    };
+
+    var lightningSpriteOptions = {
+        image:new Image(),
+        imageSrc: 'lightning_0.png'
+    };
+
     return {
         colors: colors,
         gridSize: gridSize,
         svgSize: svgSize,
         lineSize: lineSize,
-        squareSize: squareSize
+        squareSize: squareSize,
+        lightningSize: lightningSize,
+        lightningCoordinates: lightningCoordinates,
+        lightningFrameOptions: lightningFrameOptions,
+        lightningUpdates: lightningUpdates,
+        lightningSpriteOptions: lightningSpriteOptions
     };
 }());
