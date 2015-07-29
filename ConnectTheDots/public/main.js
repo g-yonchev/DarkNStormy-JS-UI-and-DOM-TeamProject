fddfd555b144d79f0c47238ddcd5d753f0b439cf
g-yonchev <georgi.d.yonchev@gmail.com>
2015-07-29 18:29:13 +0300

    Cross browsers IE and Firefox

--- ConnectTheDots/public/main.js
+++ ConnectTheDots/public/main.js
@@ -196,11 +196,18 @@ someSocket.on('set up game', function () {
                     var y = $position.top;
 
                     var browser = navigator.checkBrowser;
-                    //alert(browser);
+                    alert(browser);
+                    alert('x: ' + x + ' y: ' + y);
                     if (browser === 'Firefox 39') {
                         x = x -490;
                         y = y + 10;
                         //alert('mozila')
+                    } else if (browser === 'MSIE 10') {
+                        x = x - 588.7037353;
+                        y = y + 3 - 0.32638931;
+                    } else if (browser === 'IE 11') {
+                        x = x - 641;
+                        y = y + 2.5;
                     }
 
                     //alert('x: ' + x + ' y: ' + y);
